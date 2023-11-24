6769578e38e3cede132e824a4f5537427dbaf57a
mazmann <sadi.lundberg@hotmail.com>
2023-11-24 17:34:52 +0100

    main app done

---
+++ src/AfricaRegion.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+const AfricaRegion = () => {
+    useEffect(() => {
+        let getAfrica = async () => {
+            let response = await fetch("")
+        }
+    })
+}
+
+export default AfricaRegion;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css"
+import AfricaRegion from "./AfricaRegion"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+let [chooseRegion, setChooseRegion] = useState(false);
+return (
+  <div className="App">
+    <h1>React Country Flag Practice Application</h1>
+    <p>Choose between following regions below.</p>
+    
+    <button onClick={() => {
+      setChooseRegion(true);
+    }}
+    >
+      Africa
+      </button>
+      {chooseRegion && <AfricaRegion />}
+    <button onClick={() => {
+      setChooseRegion(true);
+    }}
+    >
+      Asia
+      </button>
+    <button onClick={() => {
+      setChooseRegion(true);
+    }}
+    >
+    Europe
+    </button>
+
+  </div>
+)
 }
 
-export default App;
+
+
