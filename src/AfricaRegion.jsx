621c594cffff90a50077b564d8218f074302f745
mazmann <sadi.lundberg@hotmail.com>
2023-11-25 19:18:25 +0100

    different if

--- src/AfricaRegion.jsx
+++ src/AfricaRegion.jsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react";
 const AfricaRegion = () => {
     let [africanCountries, setAfricanCountries] = useState([]);
     let [selectedCountry, setSelectedCountry] = useState(null);
-    let toggleParagraph = (country) => {
-        setSelectedCountry(selectedCountry === country ? null : country);
+
+    // let toggleParagraph = (country) => {
+    //     setSelectedCountry(selectedCountry === country ? null : country);
+    //   };
+
+      let toggleParagraph = (country) => {
+        if (selectedCountry === country) {
+          setSelectedCountry(null);
+        } else {
+          setSelectedCountry(country);
+        }
       };
     useEffect(() => {
         let getAfrica = async () => {
             let response = await fetch("https://restcountries.com/v3.1/region/africa")
             let json = await response.json();
-            console.log(json);
             setAfricanCountries(json);
         };
         getAfrica();
@@ -23,7 +31,6 @@ const AfricaRegion = () => {
                 src={country.flags.png}
                 alt="country"
                 onClick={() => {
-                  console.log(country.name);
                   toggleParagraph(country);
                 }}
               />
