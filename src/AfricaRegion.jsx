38ec12209d28f06cd746812e57959d0ac08b74bd
mazmann <sadi.lundberg@hotmail.com>
2023-11-25 21:07:47 +0100

    more css

--- src/AfricaRegion.jsx
+++ src/AfricaRegion.jsx
@@ -3,17 +3,14 @@ const AfricaRegion = () => {
     let [africanCountries, setAfricanCountries] = useState([]);
     let [selectedCountry, setSelectedCountry] = useState(null);
 
-    // let toggleParagraph = (country) => {
-    //     setSelectedCountry(selectedCountry === country ? null : country);
-    //   };
-
-      let toggleParagraph = (country) => {
+    let toggleParagraph = (country) => {
         if (selectedCountry === country) {
-          setSelectedCountry(null);
+            setSelectedCountry(null);
         } else {
-          setSelectedCountry(country);
+            setSelectedCountry(country);
         }
-      };
+    };
+
     useEffect(() => {
         let getAfrica = async () => {
             let response = await fetch("https://restcountries.com/v3.1/region/africa")
@@ -23,30 +20,31 @@ const AfricaRegion = () => {
         getAfrica();
 
     }, []);
+    
     return (
         <div className="flagContainer">
-          {africanCountries.map((country, i) => (
-            <div className="eachCountry" key={i}>
-              <img
-                className="eachFlag"
-                id="chooseFlag"
-                src={country.flags.png}
-                alt="country"
-                onClick={() => {
-                  toggleParagraph(country);
-                }}
-              />
-              {selectedCountry === country && (
-                <div className="countryInfo">
-                <p>Name: {country.name.common}</p>
-                <p>Capital: {country.capital}</p>
-                <p>Population: {country.population}</p>
+            {africanCountries.map((country, i) => (
+                <div className="eachCountry" key={i}>
+                    <img
+                        className="eachFlag"
+                        id="chooseFlag"
+                        src={country.flags.png}
+                        alt="country"
+                        onClick={() => {
+                            toggleParagraph(country);
+                        }}
+                    />
+                    {selectedCountry === country && (
+                        <div className="countryInfo">
+                            <p>Name: {country.name.common}</p>
+                            <p>Capital: {country.capital}</p>
+                            <p>Population: {country.population}</p>
+                        </div>
+                    )}
                 </div>
-              )}
-            </div>
-          ))}
+            ))}
         </div>
-      );
-    };
+    );
+};
 
 export default AfricaRegion;
